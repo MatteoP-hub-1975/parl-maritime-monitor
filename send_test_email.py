7bbf25c5c2a7b6e88174078d945e518384ae2c9e
MatteoP-hub-1975 <matteopellizzari@yahoo.it>
2026-03-09 16:35:19 +0100

    Update send_test_email.py

--- send_test_email.py
+++ send_test_email.py
@@ -99,7 +99,7 @@ def format_line(idx: int, item: Dict[str, str]) -> str:
 def build_report(days_back: int = 7, legislatura: str = "19"):
     kb = load_kb("kb.yaml")
 
-      ddl_items = []
+    ddl_items = []
 
     try:
         sindisp_items = fetch_recent_sindisp(days_back=days_back, legislatura=legislatura)
